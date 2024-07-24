edd076f30b2b596ff389ff64d2b0e4fcd6cc8f6a
merlina-wizard <merlini.genshin@gmail.com>
2024-07-24 14:38:48 +0200

    01

--- academy-restful-web-service-consumer/src/app/module/academy/create/create.component.ts
+++ academy-restful-web-service-consumer/src/app/module/academy/create/create.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { academy_service_token, AcademyServiceI } from '../academyI.service';
+import { AcademyService } from '../academy.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  providers:[{provide:academy_service_token,useClass:AcademyService}],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+
+  private router:Router = new Router();
+
+  form!: FormGroup;
+
+  private academyService = inject<AcademyServiceI>(academy_service_token);
+
+  submit() {
+
+    this.academyService.saveAcademy(this.form.value).subscribe((res: any) => {
+      console.log('Academy created successfully!');
+      this.router.navigateByUrl('academy/index');
+    })
+
+  }
+
+
+  ngOnInit() {
+    this.form = new FormGroup({
+     code: new FormControl(''),
+     title: new FormControl(''),
+     cityLocation: new FormControl(''),
+     studentsNumber: new FormControl(0)
+    });
+
+  }
 
 }
--- academy-restful-web-service-consumer/src/app/module/academy/edit/edit.component.ts
+++ academy-restful-web-service-consumer/src/app/module/academy/edit/edit.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { academy_service_token, AcademyServiceI } from '../academyI.service';
+import { AcademyService } from '../academy.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Academy } from '../../../model/Academy.model';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
+  providers: [{ provide: academy_service_token, useClass: AcademyService }],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css'
 })
-export class EditComponent {
+export class EditComponent implements OnInit {
 
+  code! : string;
+
+  form!: FormGroup;
+
+  academy!: Academy;
+
+
+  private academyService = inject<AcademyServiceI>(academy_service_token);
+
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  submit() {
+
+    this.academyService.updateAcademy(this.form.value).subscribe((res: any) => {
+      console.log('Academy updated successfully!');
+      this.router.navigateByUrl('academy/index');
+    })
+
+  }
+  ngOnInit(): void {
+
+    //recupro il codice dell'academy da modificare
+    this.code = this.route.snapshot.params['code'];
+    //invochiamo il metodo  getAcademyByCode per farci restituire l'academy selezionata
+    this.academyService.getAcademyByCode(this.code).subscribe((data) => {
+      //trasformazione in oggetto academy
+      this.academy = data;
+      console.log(this.academy);
+    });
+
+      this.form = new FormGroup({
+       code: new FormControl(''),
+       title: new FormControl(''),
+       cityLocation: new FormControl(''),
+       studentsNumber: new FormControl()
+  });
+}
 }
